3e9808ca1579f4814b2a480b1df1ece4c1a7d62d
Roy <royyi2006@gmail.com>
2026-01-13 11:58:03 -0800

    completed the model training, testing, saving, loading process

--- pytorch_workflow.py
+++ pytorch_workflow.py
@@ -17,6 +17,7 @@
 import torch
 from torch import nn #NOTE: nn contains all of pytorch's building blocks for neural networks
 import matplotlib.pyplot as plt
+import numpy as np
 
 #check PyTorch version
 print(torch.__version__)
@@ -160,7 +161,7 @@ plot_predictions(predictions=y_preds)
 loss_fn = nn.L1Loss() #L1 loss is just the absolute error between the expected value and the input value
 
 #set up a optimizer 
-optimzer = torch.optim.SGD(params=model_0.parameters(), lr=0.01) #stochastic gradient descent, perform gradient descent by randomly adjusting values and following a direction that lowers the loss function
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.0001) #stochastic gradient descent, perform gradient descent by randomly adjusting values and following a direction that lowers the loss function
                                                           #lr (learnig rate) is possibily the most important hyperparameter you can set, just how big of a step in the param you can take
 ### building a training loop
 
@@ -172,14 +173,147 @@ optimzer = torch.optim.SGD(params=model_0.parameters(), lr=0.01) #stochastic gra
 # 5. optimizer step 0- use the optimizer to adjust outr model's parametrs to improve the loss. (gradient descent)
 #
 #NOTE: an epoch is one loop through the data (this is a hyperparameter because we set it ourselves)
-epochs = 1
+epochs = 18000
+#NOTE: if decreasing the learning rate, then the number of epochs need to be increased to account for the increased number of steps requireed to reach the goal
+##training
 
-#0. loop through the data
+
+#track different values so you can compare past and future experiments
+epoch_count = []
+loss_values = []
+test_loss_values = []
+
+#0. loop through the data / step through the model for a number of epochs
 for epoch in range(epochs):
-    model_0.train() #train mode in PyTorch set all parameters taht require gradients to require gradients
 
-    model_0.eval() #turns off graident tracking
+    model_0.train() #train mode in PyTorch set all parameters taht require gradients to require gradients
+    
+    #1. forward pass
+    # calls the forward method defined in the model declaration
+    y_pred = model_0(x_train)
+
+    #2. calculate the loss / how diffrent are the models predictions to the true values
+    loss = loss_fn(y_pred, y_train) #(input, target)
+
+    #3. optimizer zero grad/ zero the gradients of the optimizer
+    optimizer.zero_grad()
+
+    #4. perform backpropagation on the loss with respect to the parameteres of the model/ compute the gradient of every parameter with requires grad = true
+    loss.backward()
+
+    #5. step the optimizer (perform gradient descent) / update teh model's paremeters with respect to the gradients calculated by loss.backward()
+    optimizer.step() #NOTE: by default, how the optimizer changes will accumulate through the loop, so we have to zero them above in step three for the next iteration of the loop
+
+
+    #tell the model we're evaluating now
+    model_0.eval() #turns off different settings in the model not needed for evaluation/testing (dropout/batch norm layers)
+
+    with torch.inference_mode(): # turns off gradient tracking
+        #1. Do the forward pass in the 
+        test_pred = model_0(x_test)
+
+        #2. calculate the loss
+        test_loss = loss_fn(test_pred, y_test)
+    
+    if epoch % 1000 == 0: #dont gotta print every epoch, every 10 or any other intervals are equally valid
+        epoch_count.append(epoch)
+        loss_values.append(loss)
+        test_loss_values.append(test_loss)
+        print(f"Epoch: {epoch} | Loss: {loss} | Test_loss: {test_loss}")
+        print(model_0.state_dict())
 
 
  
 
+with torch.inference_mode():
+    y_preds_new = model_0(x_test)
+
+plot_predictions(predictions=y_preds)
+plot_predictions(predictions=y_preds_new)
+print(model_0.state_dict())
+
+print(epoch_count, loss_values, test_loss_values)
+
+#plot the loss curves
+#dont forget to convert the values to be numpy useable before using matplotlib, which requires the tensor to be converted to numpy formate and the values be on the cpu instead of gpu
+def plot_loss_curve():
+    plt.figure() #this just creates a new figure instead of plotting on top of the old figure
+    plt.plot(epoch_count, np.array(torch.tensor(loss_values).cpu().numpy()), label="Train loss")
+    plt.plot(epoch_count, np.array(torch.tensor(test_loss_values).cpu().numpy()), label="Test loss")
+    plt.title("training and test loss curves")
+    plt.ylabel("Loss")
+    plt.xlabel("epochs")
+    plt.legend()
+    plt.savefig("testLF.png")
+
+plot_loss_curve()
+
+#TODO: writing code to save a python model
+
+#NOTE: there are three main methods for saving and loading in PyTorch.
+#1. torch.save(), saves the model in python pickle format, pickle implemnets a binary protocl for serialize and de-serializing a python object structure, pickling is the process whereby a python object hierarchy is converted into a byte stream, and unpickling is the inverse operation, this is also known as serilizing, marshalling or flattening
+#2. torch.load(), allows you to load a saved PyTorch object
+#3. torch.nn.Module.load_state_dict() , this allows to load a model's saved state dictionary
+# a state_dict is the learnable parameters, its a dictionary object that maps each layer to its parameter tensors, only learnable parameters, registered buffers and optimizers objects jave entries in teh state dict, with the latter containing information about the optimizer's state as well as the hyperparameters used
+
+#NOTE: there are two ways to save, either save the entire model or just the state dict
+ # - if only saving the state_dict, you would need to recreate the model instance- this is also the smaller file option
+ # - if saving the entire model, you would need to exactly recreate the entire file structure including the source code of the model on the target syste,.
+
+#saving the PyTorch Model
+
+from pathlib import Path
+
+#1. create a model directory
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+#2. create a model save path
+MODEL_NAME = "01_pytorch_workflow_model_0.pth" #pytorch objects usually have the extension as .pt or .pth 
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+print(MODEL_SAVE_PATH)
+
+#3. save the model_state_dict()
+
+print(f"saving model to : {MODEL_SAVE_PATH}")
+torch.save(obj=model_0.state_dict(), f=MODEL_SAVE_PATH)
+#torch.save(obj (saved object), 
+#          f (a file like object, has to implement write and flush, or a sting or os.PathLike object cintaining file name),
+#          pickle_module=pickle,
+#          pickle_protocol=DEFAULT_PROTOCOL, (module used for pickling metadata and objects)
+#          _use_new_zipfile_serialization=True) can be specified to override the default protocol
+#
+
+
+#TODO: loading a model from a file
+
+#torch.load (f, map_location=None, pickle_module=pickle, **pickle_load_args) uses python's unpickling facilities but treates storages, which underlie tensors, sepcially. they are first
+#deserialized on the cpu and are then moved to the devices they were saved from. if no such device available it will raise exception, optionof dynamically remappeing to a alternative set of devices using the map_location if devices not availablle
+
+# since we saved the state_dict() rather than the entire model, we will create a new instance of our model class and load the saved state_dict() into that.
+
+#to load in a saved state_dict() we have to instantiate a new instance of out model class
+
+loaded_model_0 = LinearRegressionModel()
+
+#load the saved state_dict() of model_0, this will update the new instance with the updated parameters
+
+loaded_model_0.load_state_dict(torch.load(f=MODEL_SAVE_PATH))
+print(loaded_model_0.state_dict())
+
+#make some predictions with out loaded model
+
+loaded_model_0.eval()
+
+with torch.inference_mode():
+    loaded_model_preds = loaded_model_0(x_test)
+
+print(loaded_model_preds)
+
+model_0.eval()
+
+with torch.inference_mode():
+    y_preds = model_0(x_test)
+
+print(y_preds == loaded_model_preds)
